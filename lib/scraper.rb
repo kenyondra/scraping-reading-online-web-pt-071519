557353430e75cc1377139fa7f3d849f693966c2a
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-09-20 05:03:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,13 @@
-require 'nokogiri'
-require 'open-uri'
+require "nokogiri"
+require "open-uri"
+require "pry"
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc.css(".headline-26OIBN")
+
+binding.pry 
 
 
 
